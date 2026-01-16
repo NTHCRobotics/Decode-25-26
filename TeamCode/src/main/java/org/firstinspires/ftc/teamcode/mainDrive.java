c8224ccd4d88612156fc101df35eb7fd220f30c3
nbala123 <nbala@newtech.coppellisd.com>
2026-01-16 00:48:18 -0600

    fixed color implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainDrive.java
@@ -15,12 +15,17 @@ public class mainDrive extends OpMode{
     //Hardware
     private DcMotorEx wheelFL, wheelFR, wheelBL, wheelBR, intakeB; //Motors
     private Servo magazineServo, loadServo; //Servos
-    private NormalizedColorSensor colorSensor; //Sensors
+    private NormalizedColorSensor colorSensorL; //Left Color Sensor
+    private NormalizedColorSensor colorSensorBR; //Right Back Color Sensor
+    private NormalizedColorSensor colorSensorFR; //Right Front Color Sensor
+
+
 
     //Variables
     double speedMod = 0.5; //Speed of wheel motors (around 1/2 maximum rate)
     double[] magazineReadPositions = {0.0, 360.0/3/300, 360.0/3/300*2}; //lists out 1/3rd rotations of the magazine. The weird math is to normalize it to [0,1] given the servo's 300 degree range.
     // !IMPORTANT! artifactOrder is an ArrayList since it needs to be repeatedly scanned and edited.
+    // 0 --> L, 1 --> BR, 2 --> FR
     ArrayList<String> magazineOrder = new ArrayList<String>(); //correlates to the artifact color currently in readPositions (e.g. the artifacts in the magazine)
 
     String[] obeliskOrder = {null, null, null}; //Correlates to the competition artifact sequence (read at init(), never changed)
@@ -39,7 +44,9 @@ public class mainDrive extends OpMode{
         magazineServo = hardwareMap.get(Servo.class, "magazineServo");
         loadServo = hardwareMap.get(Servo.class, "loadServo");
         //Sensors
-        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensorL");
+        colorSensorL = hardwareMap.get(NormalizedColorSensor.class, "colorSensorL");
+        colorSensorBR = hardwareMap.get(NormalizedColorSensor.class, "colorSensorBR");
+        colorSensorFR = hardwareMap.get(NormalizedColorSensor.class, "colorSensorFR");
 
         //Initialization
         magazineServo.setPosition(0.0);
@@ -56,6 +63,7 @@ public class mainDrive extends OpMode{
     public void loop(){
         drive(); // translation and rotation
         //spinIntakes(); //Spinning the intakes (duh) DISABLED UNTIL BUTTON IS BOUND
+        indexArtifacts(); // Keeps a running list of what artifacts exist within the magazine
         updateTelemetry(); // updates driver's hub telemetry
     }
 
@@ -88,7 +96,7 @@ public class mainDrive extends OpMode{
     }
 
     //Color sensor
-    public double[] colorMatch(NormalizedColorSensor colorSensor){
+    public String colorMatch(NormalizedColorSensor colorSensor){
         double red = colorSensor.getNormalizedColors().red;
         double green = colorSensor.getNormalizedColors().green;
         double blue = colorSensor.getNormalizedColors().blue;
@@ -96,24 +104,28 @@ public class mainDrive extends OpMode{
         double purpleMatch = Math.max(0, Math.min(red, blue) - green - Math.abs(red - blue));
         double greenMatch = Math.max(0, green - Math.max(red, blue));
 
-        return new double[] {purpleMatch, greenMatch};
+        double purpleTolerance = 0.5;
+        double greenTolerance = 0.5;
+
+        if (purpleMatch < purpleTolerance && greenMatch < greenTolerance){
+            return "empty";
+        }
+        else if (purpleMatch > greenMatch){
+            return "purple";
+        }
+        else {
+            return "green";
+        }
     }
 
-    public void indexArtifacts() throws InterruptedException {
-        for (int i = 0; i < 3; i++){
-            magazineServo.setPosition(magazineReadPositions[i]);
-            sleep(200); //Allows for the magazine to rotate
-            double[] color = colorMatch(colorSensor);
-            if (color[0] < 0.5 && color[1] < 0.5){
-                magazineOrder.set(i, "empty");
-            }
-            else if (color[0] > color[1]){
-                magazineOrder.set(i, "purple");
-            }
-            else {
-                magazineOrder.set(i, "green");
-            }
-        }
+    public void indexArtifacts() {
+        String colorL = colorMatch(colorSensorL);
+        String colorBR = colorMatch(colorSensorBR);
+        String colorFR = colorMatch(colorSensorFR);
+
+        magazineOrder.set(0, colorL);
+        magazineOrder.set(1, colorBR);
+        magazineOrder.set(2, colorFR);
     }
 
     public void launchInOrder() throws InterruptedException {
@@ -127,7 +139,7 @@ public class mainDrive extends OpMode{
     }
 
     public void updateTelemetry() {
-
+        telemetry.addData("Magazine Order", magazineOrder.get(0) + " " + magazineOrder.get(1) + " " + magazineOrder.get(2));
     }
 
 }
